98b537776f49e073708bb7cb82c394811e27857b
Hunter Miller <mycalhunter@gmail.com>
2021-02-13 21:15:55 -0500

    day 1 commit

--- day1/test.js
+++ day1/test.js
@@ -1,3 +1,25 @@
-// initial commit setting up GitHub on VS Code
-// change and push with git bash
-// commit using git bash in VS code terminal
+function inputCheck() {
+    // variables
+    let form = document.getElementById("form");
+    let minNum = document.getElementById("minNum").value;
+    let maxNum = document.getElementById("maxNum").value;
+    let num = document.getElementById("inputNum").value;
+    let output = document.getElementById("output");
+
+    // compute
+    if (num && minNum && maxNum) { // if all inputs have a value
+        if ((Number(num) >= Number(minNum)) && (Number(num) <= Number(maxNum))) { // if number is between min & max
+            output.innerHTML = num + " is between " + minNum + " and " + maxNum;
+        } else { // if number is out of range
+            output.innerHTML = num + " is <span class='issue'>NOT</span> between " + minNum + " and " + maxNum;
+        } 
+    } else { // if any input is empty
+        output.innerHTML = "<span class='issue'>Fill out all sections</span>";
+    }
+
+    // prevent reload
+    function handleForm(event) {
+        event.preventDefault();
+    }
+    form.addEventListener('submit', handleForm);
+}
